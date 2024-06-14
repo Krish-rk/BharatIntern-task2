269e3bb79085627d4c62280f51c325db0024bdb1
Krish-rk <irohithkrishnan2003@gmail.com>
2024-06-14 23:32:00 +0530

    first commit

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const Article = require('./models/article')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const app = express()
+const dotenv = require("dotenv");
 
-mongoose.connect('mongodb://localhost:27017/BlogWebsiteDatabase')
+dotenv.config(); // Ensure this is at the top
+console.log(dotenv.config());
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
 
+mongoose.connect(`mongodb+srv://${username}:${password}@regform.gqif0d0.mongodb.net/?retryWrites=true&w=majority&appName=RegForm`, {
+    serverSelectionTimeoutMS: 5000, 
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
@@ -14,7 +25,7 @@ app.get('/',async(req,res) => {
     const articles = await Article.find().sort({createAt:'desc'})
     res.render('articles/index',{articles:articles})
 })
-
+ 
 app.use('/articles',articleRouter)
 
-app.listen(5500)
+app.listen(4949)
